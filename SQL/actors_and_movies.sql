4531d73c3a8114579d77ec7e2efaa74cdb81f8bf
Devesh Singh <71709216+Devesh0110@users.noreply.github.com>
2021-07-21 18:56:52 +0530

    Update actors_and_movies.sql

--- SQL/actors_and_movies.sql
+++ SQL/actors_and_movies.sql
@@ -1,3 +1,6 @@
+--SCHEMA - https://www.interviewbit.com/problems/actors-and-their-movies/
+
+
 select movies.movie_title from movies inner join movies_cast on movies.movie_id=movies_cast.movie_id
 inner join actors on actors.actor_id=movies_cast.actor_id 
 WHERE movies_cast.actor_id IN (SELECT actor_id FROM movies_cast GROUP BY actor_id HAVING COUNT(actor_id) > 1);
