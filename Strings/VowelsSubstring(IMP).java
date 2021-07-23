2a35bdc1f2b4612308ca603947c01b33fd192267
Devesh Singh <71709216+Devesh0110@users.noreply.github.com>
2021-07-23 23:36:55 +0530

    Update VowelsSubstring(IMP).java

--- Strings/VowelsSubstring(IMP).java
+++ Strings/VowelsSubstring(IMP).java
@@ -1,3 +1,32 @@
+Given a string A consisting of lowercase characters.
+
+You have to find the number of substrings in A which starts with vowel and end with consonants or vice-versa.
+
+Output Format
+Return a integer denoting the the number of substrings in A which starts with vowel and end with consonants or vice-versa with modulo 109 + 7.
+
+
+
+Example Input
+Input 1:
+
+ A = "aba"
+Input 2:
+
+ A = "a"
+
+
+Example Output
+Output 1:
+
+ 2
+Output 2:
+
+ 0 
+     
+     
+
+
 public class Solution {
     public int solve(String A) {
         // Count the number of vowels and the number of consonants and return their product
