a53c8677004682984a258c2a0d3699579139d9d5
Devesh Singh <71709216+Devesh0110@users.noreply.github.com>
2021-07-23 23:03:10 +0530

    Update Palindrome(IMP).java

--- Strings/Palindrome(IMP).java
+++ Strings/Palindrome(IMP).java
@@ -1,6 +1,27 @@
 //THIS CODE PASSES ALL TEST CASES
 //MY CODE FAILED TIME COMPLEXITY IN CASE OF INFINITE
 
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Example:
+
+"A man, a plan, a canal: Panama" is a palindrome.
+
+"race a car" is not a palindrome.
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+
+
+
+
+
+
+
+
+
+
 public class Solution {
 	public int isPalindrome(String a) {
 	    // check if char at i = char at n - i with two pointers, ignoring non alphanumeric
