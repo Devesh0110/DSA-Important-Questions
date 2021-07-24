e9ce7edb990f2653211651f1ad2eef8886de148f
Devesh Singh <71709216+Devesh0110@users.noreply.github.com>
2021-07-24 23:40:10 +0530

    Update ConsecutiveDuplicates.java

--- Strings/ConsecutiveDuplicates.java
+++ Strings/ConsecutiveDuplicates.java
@@ -1,3 +1,37 @@
+Problem Description
+
+Given a string A and integer B, remove all consecutive same characters that have length exactly B.
+    
+Input Format
+First Argument is string A.
+Second argument is integer B.
+
+Output Format
+Return a string after doing the removals.
+
+Example Input
+Input 1:
+
+A = "aabcd"
+B = 2
+    
+Input 2:
+
+A = "aabbccd"
+B = 2
+    
+Example Output
+Output 1:
+
+ "bcd"
+Output 2:
+
+ "d"
+
+
+
+
+
 public class Solution {
     public String solve(String A, int B) {
        int len = A.length();
