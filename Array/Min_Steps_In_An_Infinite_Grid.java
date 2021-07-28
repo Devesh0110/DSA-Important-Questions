b77891209520c40085d8e073917b42eac8ec0c28
Devesh Singh <71709216+Devesh0110@users.noreply.github.com>
2021-07-28 17:27:35 +0530

    Update Min_Steps_In_An_Infinite_Grid.java

--- Array/Min_Steps_In_An_Infinite_Grid.java
+++ Array/Min_Steps_In_An_Infinite_Grid.java
@@ -1,3 +1,44 @@
+nput Format
+Given two integer arrays A and B, where A[i] is x coordinate and B[i] is y coordinate of ith point respectively.
+
+
+
+Output Format
+Return an Integer, i.e minimum number of steps.
+
+
+
+Example Input
+Input 1:
+
+ A = [0, 1, 1]
+ B = [0, 1, 2]
+
+
+Example Output
+Output 1:
+
+ 2
+
+
+Example Explanation
+Explanation 1:
+
+ Given three points are: (0, 0), (1, 1) and (1, 2).
+ It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).
+
+
+
+     
+     
+     
+     
+     
+     
+     
+
+
+
 public class Solution {
     public int coverPoints(int[] x, int[] y) {
          int n=x.length,ans=0;
