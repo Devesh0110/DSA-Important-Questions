2900fbc9582b891161bae0eaff71e0b699d97a2b
Devesh Singh <71709216+Devesh0110@users.noreply.github.com>
2021-08-09 12:19:52 +0530

    Update SumOfNumbersInString.java

--- Strings/SumOfNumbersInString.java
+++ Strings/SumOfNumbersInString.java
@@ -1,3 +1,27 @@
+Given a string str containing alphanumeric characters. The task is to calculate the sum of all the numbers present in the string.
+
+Example 1:
+
+Input:
+str = 1abc23
+Output: 24
+Explanation: 1 and 23 are numbers in the
+string which is added to get the sum as
+24.
+Example 2:
+
+Input:
+str = geeks4geeks
+Output: 4
+Explanation: 4 is the only number, so the
+sum is 4.
+
+
+
+
+
+
+
 class Solution
 {
     //Function to calculate sum of all numbers present in a string.
