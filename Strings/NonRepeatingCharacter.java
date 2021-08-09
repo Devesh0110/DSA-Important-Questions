17e830636f28d60484d8ed02316d19d67728fdaf
Devesh Singh <71709216+Devesh0110@users.noreply.github.com>
2021-08-09 09:59:03 +0530

    Update NonRepeatingCharacter.java

--- Strings/NonRepeatingCharacter.java
+++ Strings/NonRepeatingCharacter.java
@@ -1,3 +1,26 @@
+Given a string S consisting of lowercase Latin Letters. Find the first non-repeating character in S.
+
+Example 1:
+
+Input:
+S = hello
+Output: h
+Explanation: In the given string, the
+first character which is non-repeating
+is h, as it appears first and there is
+no other 'h' in the string.
+Example 2:
+
+Input:
+S = zxvczbtxyzvy
+Output: c
+Explanation: In the given string, 'c' is
+the character which is non-repeating. 
+
+
+
+
+
 class Solution
 {
     //Function to find the first non-repeating character in a string.
